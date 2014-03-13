4c13fbf28195eb4e6148200b8158b7f935a90d4e
Marc Planard <case@corpsmoderne.net>
2014-03-13 09:41:20 +0100

    add license header

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+//    Copyright (C) 2014 by Marc Planard and Le Cortex
+//
+//    This file is part of Flappy Space Program.
+//
+//    Flappy Space Program is free software: you can redistribute it and/or 
+//    modify it under the terms of the GNU Affero General Public License as
+//    published by the Free Software Foundation, either version 3 of the 
+//    License, or (at your option) any later version.
+//
+//    Flappy Space Program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Affero General Public License for more details.
+//
+//    You should have received a copy of the GNU Affero General Public License
+//    along with Flappy Space Program.  
+//    If not, see <http://www.gnu.org/licenses/>.
+
 $(document).ready(function() {
   var canvas = $("#canvas");
   var ctx = canvas[0].getContext("2d");
